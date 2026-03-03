e5d33a19641e7a660458744419b9fd9a0e04f3c3
youtunfreelance-hue <youtun.freelance@gmail.com>
2026-03-03 21:14:43 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-
-const config: Config = {
+const config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
